02b4660aee287f36f84e6656a7b279c8da4bf5d3
SashanthSajith <81414661+SashanthSajith@users.noreply.github.com>
2021-10-05 19:56:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ function setup(){
     canvas.center();
 }
 function preload(){
-    img = loadImage('laptop.jpg');
+    img = loadImage('laptop and tab image.jpg');
 }
 function draw(){
     image(img, 0, 0, 640, 420);
     fill("#00FF00");
-    text("Laptop", 45, 75);
+    text("Laptop", 220, 40);
     noFill();
     stroke("#00FF00");
-    rect(20, 30, 550, 450);
+    rect(220, 50, 350, 300);
+    fill("#00FF00")
+    text("Tablet", 70, 95);
+    noFill();
+    stroke("#00FF00");
+    rect(70, 100, 220, 220);
 }
